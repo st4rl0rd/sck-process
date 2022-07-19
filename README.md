9811c5a0cc8596ba994aed3867db2225a4cb9a9a
wilder ibañez <wibanezc@gmail.com>
2022-07-18 20:26:55 -0400

    rest server

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sock
+Construir node
+```
+npm install
+```
+
+Generar el DIST
+```
+tsc -w
+```
+
+Levantar servidor
+```
+nodemon/node dist/
+```
---
+++ routes/router.ts
@@ -0,0 +1,24 @@
+import { Router, Request, Response } from "express";
+
+export const router = Router();
+
+router.get('/mensajes',( req: Request, res: Response ) => {
+  res.json({
+    ok: true,
+    mensaje: 'Todo esta bien'
+  });
+});
+
+router.post('/mensajes:id',( req: Request, res: Response ) => {
+  const cuerpo = req.body.cuerpo;
+  const de = req.body.de;
+  const id = req.params.id;
+  res.json({
+    ok: true,
+    cuerpo: cuerpo,
+    de: de,
+    id
+  });
+});
+
+export default router;
